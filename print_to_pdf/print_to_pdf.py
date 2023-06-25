20cd592274ae2050ed333486ea8c47090fa9adda
vimi <vimi.vimi@dell>
2023-06-25 20:48:27 +0200

    up

--- print_to_pdf/print_to_pdf.py
+++ print_to_pdf/print_to_pdf.py
@@ -1,20 +1,52 @@
-# please write me a flask app for printing to pdf using flask-weasyprint. Make simple page with 3 columns. column 1 is sidebar. column 2 is content. column 3 is also content with loremipsum text. In navbar make button for "print to pdf". But i want only print to pdf third column!
+from flask import Flask, render_template, make_response, render_template_string
+from flask_weasyprint import HTML, CSS
 
-# The `index()` function renders the `index.html` template which contains the three columns. The `print_pdf()` function generates a PDF of the third column by first rendering the entire page with `print_pdf=True` and then using Flask-WeasyPrint's `render_pdf()` function to generate the PDF.
-from flask import Flask, render_template, Response
-from flask_weasyprint import HTML, render_pdf
+# app = Flask(__name__)
+# app.config['WEASYPRINT_FONT_CONFIG'] = '/usr/share/fonts/truetype/msttcorefonts/Arial.ttf'
+#
+# @app.route('/')
+# def index():
+#     return render_template('index.html')
+#
+# @app.route('/pdf')
+# def generate_pdf():
+#     css = CSS(string='@page { size: letter; margin: 1in; }')
+#     response = make_response(HTML(string=render_template_string('{% extends "index.html" %}{% block content %}'
+#                                                                 '{% filter replace(" \\n", "") %}'
+#                                                                 '.col-md-6:not(:nth-child(2)) { display: none; }'
+#                                                                 '.col-md-6:nth-child(2) { width: 100%; }'
+#                                                                 '{% endfilter %}{% endblock %}')).write_pdf(stylesheets=[css]))
+#     response.headers['Content-Type'] = 'application/pdf'
+#     response.headers['Content-Disposition'] = 'attachment; filename=column2.pdf'
+#     return response
+#
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
+from weasyprint import default_url_fetcher
+
 
 app = Flask(__name__)
+# app.config['WEASYPRINT_FONT_CONFIG'] = '/usr/share/fonts/truetype/msttcorefonts/Arial.ttf'
+# app.config['WEASYPRINT_URL_FETCHER'] = default_url_fetcher
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/print')
-def print_pdf():
-    html_string = render_template('print_version.html')
-    return Response(render_pdf(HTML(string=html_string, base_url=request.host_url)),
-                    mimetype='application/pdf')
+
+@app.route('/pdf')
+def generate_pdf():
+    # css = CSS(string='.col-md-6:not(:nth-child(2)) { display: none; } .col-md-6:nth-child(2) { width: 100%; }')
+    css = CSS(string='.do-not-print { display: none; } .print-this-pdf { width: 100%; }')
+    response = make_response(HTML(string=render_template('index.html')).write_pdf(stylesheets=[css], presentational_hints=True, attachments=None, styles=None, font_config=None))
+    response.headers['Content-Type'] = 'application/pdf'
+    # response.headers['Content-Disposition'] = 'attachment; filename=column2.pdf'
+    response.headers['Content-Disposition'] = 'inline; filename=column2.pdf'
+    return response
+
 
 if __name__ == '__main__':
     app.run(debug=True)
+
